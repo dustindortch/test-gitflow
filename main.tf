3f650df02489a79b4cc7b144aeec26a718ed7b3e
Dustin Dortch <25554532+dustindortch@users.noreply.github.com>
2022-04-20 09:59:45 -0400

    Update main.tf (#2)

--- main.tf
+++ main.tf
@@ -6,3 +6,12 @@ locals {
 output "name" {
   value = "${local.first_name} ${local.last_name}"
 }
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "tf-cloud-test"
+  location = "eastus"
+}
